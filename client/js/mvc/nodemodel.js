91bc3e963b8c551da1a571b4ee59a338d35e59ce
Damien <damien@damien-System-Product-Name.(none)>
2013-05-27 14:40:01 +0200

    minor changes

--- client/js/mvc/nodemodel.js
+++ client/js/mvc/nodemodel.js
@@ -1,45 +1,41 @@
+/* global Model, TreeStructure */
+
 var NodeModel = new Class({
 	Extends: Model,
 	Implements: TreeStructure,
 	name: '',
-	
+
 	initialize: function(){
 		Model.prototype.initialize.apply(this, arguments);
-		TreeStructure.prototype.initialize.call(this);
-		
+		TreeStructure.init.call(this, this.get('children'));
+
 		if( this.has('name') ) this.name = this.get('name');
-		if( this.has('children') ) this.children = this.parseChildren(this.get('children'));
 	},
-	
+
 	toString: function(){
 		return 'NodeModel';
 	},
-	
-	setChildren: function(children){		
-		this.parseChildren(children).forEach(this.appendChild, this);
-		return this;
-	},
-	
+
 	adopt: function(child, index){
 		index = typeof index != 'number' ? this.children.length : index.limit(0, this.children.length);
-		
+
 		this.insertBefore(child, this.children[index]);
 		//child.crossAll(function(node){ node.emit('enter'); }, null, true);
 		this.emit('adopt', child, index);
-		
+
 		return this;
 	},
-	
+
 	emancipate: function(){
 		this.parentNode.removeChild(this);
 		//this.crossAll(function(node){ node.emit('leave'); }, null, true);
 		this.emit('emancipate');
-		
+
 		return this;
 	},
-	
+
 	sync: function(action, args, callback){
-		
+
 	}
 });
 
