5c32263f78eeb0a121b384f1b49e36aab30da631
Damien Maillard <Angelblade@hotmail.fr>
2013-06-11 23:39:41 +0200

    require.js load without load
    require.id('foo');
    provide({foo: 'bar'});
    
    require.id('name');
    provide('foo', function(foo){
    foo.moredata = true;
    return foo;
    });
    
    require('name', function(name){
    name.foo == 'bar' -> true;
    name.moredata == true -> true
    });

--- client/js/require.js
+++ client/js/require.js
@@ -1,159 +1,207 @@
-window.require = (function(){
+function DependencyResolver(array){
+	this.index = 0;
+	this.dependencies = array;
 
-	function require(){
-		return new Resolver(Array.apply(Array, arguments));
+	if( array.length > 0 && typeof array[array.length - 1] == 'function' ){
+		this.callback = array.pop();
 	}
 
-	require.config = {
-		async: true,
-		root: './',
-		extension: 'js',
-		charset: 'utf8'
-	};
+	this.next();
+}
 
-	require.filepath = function(name){
-		return require.config.extension + '/' + name + '.' + require.config.extension;
-	};
+DependencyResolver.prototype.cache = {};
 
-	require.fileURL = function(name){
-		return require.config.root + require.filepath(name, require.config.extension);
-	};
+DependencyResolver.prototype.setId = function(id){
+	this.id = id;
+};
 
-	require.loadFile = function(path, callback){
-		var type, element;
+DependencyResolver.prototype.getId = function(id){
+	return this.id;
+};
 
-		if( path.match(/.js$/) ){
-			type = 'js';
-			element = document.createElement('script');
-			element.type = 'text/javascript';
-			element.charset = require.config.charset;
-			element.async = require.config.async;
-		}
-		else if( path.match(/.css$/) ){
-			type = 'css';
-			element = document.createElement('link');
-			element.type = 'text/css';
-			element.rel = 'stylesheet';
-		}
-		else{
-			throw new Error('unsupported file extension');
-		}
+DependencyResolver.prototype.resolve = function(id){
+	
+};
 
-		if( typeof callback == 'function' ){
-			element.onerror = callback;
-			element.onload = callback;
-		}
+DependencyResolver.prototype.hasNext = function(){
+	return this.index < this.dependencies.length;
+};
 
-		document.head.appendChild(element);
-
-		if( type == 'js' ) element.src = path;
-		else element.href = path;
-	};
-
-	require.loadFiles = function(names, extension){
-		var i = 0, j = names.length, prevExt = require.config.extension, prevAsync = require.config.async;
-
-		require.config.extension = extension;
-		require.config.async = false;
-		for(;i<j;i++){
-			require.loadFile(require.fileURL(names[i]));
-		}
-		require.config.extension = prevExt;
-		require.config.async = prevAsync;
-
-	};
-
-	function Resolver(array){
-		this.names = array;
-		this.dependencies = [];
-
-		if( array.length > 0 && typeof array[array.length - 1] == 'function' ){
-			this.callback = array.pop();
-		}
-
-		this.next();
+DependencyResolver.prototype.next = function(){
+	if( this.hasNext() ){
+		this.setId(this.dependencies[this.index]);
+		this.index++;
+		this.loadModule(this.getId());
 	}
+	else{
+		if( this.callback ) this.callback.apply(window, this.dependencies);
+		this.onend();
+	}
+};
 
-	Resolver.prototype.hasNext = function(){
-		return this.names.length > 0;
-	};
+DependencyResolver.prototype.loadModule = function(id){
+	if( id in this.cache ){
+		this.onresolve(this.cache[id]);
+	}
+	else{
+		this.resolve(id);
+	}
+};
 
-	Resolver.prototype.next = function(){
-		if( this.hasNext() ){
-			this.current = this.names.shift();
-			this.perform(this.current);
-		}
-		else{
-			if( this.callback ) this.callback.apply(window, this.dependencies);
-		}
-	};
+DependencyResolver.prototype.provide = function(){
+	if( arguments.length === 0 ){
+		this.onprovide(null);
+	}
+	else if( arguments.length === 1 ){
+		this.onprovide(arguments[0]);
+	}
+	else{
+		// sinon c'est plus complexe, il faut charger les dépendances avant de pouvoir continuer
+		var args = Array.apply(Array, arguments), callback = args.pop();
 
-	Resolver.prototype.loadModule = function(name){
-		this.path = require.fileURL(name, 'js');
+		args.push(function(){
+			if( callback ) this.onprovide(callback.apply(window, arguments));
+		}.bind(this));
 
-		if( this.path in require.cache ){
-			this.onresolve(require.cache[this.path]);
-		}
-		else{
-			require.loadFile(this.path, this.onload.bind(this));
-		}
-	};
+		new this.constructor(args);
+	}
+};
 
-	Resolver.prototype.perform = function(name){
-
-		var self = this;
-		window.provide = function(){
-			self.provided = Array.apply(Array, arguments);
-		};
-
-		this.loadModule(name);
-	};
-
-	Resolver.prototype.onload = function(e){
-		window.provide = function(){
-			throw new Error('provide should be called only once per module');
-		};
-
-		var provided = this.provided;
-		delete this.provided;
-
-		if( e && e.type == 'error' ){
-			throw new Error('error whil loading module');
-		}
-
-		if( !provided  ){
-			this.onprovide(null);
-			//throw new Error('provide not called in the file');
-		}
-		else if( provided.length == 1 ){
-			// provide({}) ou provide(function(){}) on récup direct la valeur
-			this.onprovide(provided[0]);
-		}
-		else{
-			// sinon c'est plus complexe, il faut charger les dépendances avant de pouvoir continuer
-			var callback = provided.pop();
-
-			provided.push(function(){
-				this.onprovide(callback.apply(window, arguments));
-			}.bind(this));
-
-			new Resolver(provided);
-		}
-	};
-
-	Resolver.prototype.onprovide = function(data){
-		require.cache[this.path] = data;
+DependencyResolver.prototype.onprovide = function(data){
+	this.cache[this.getId()] = data;
+	if( this.dependencies ){
 		this.onresolve(data);
-	};
+	}	
+};
 
-	Resolver.prototype.onresolve = function(data){
-		this.dependencies.push(data);
-		this.next();
-	};
+DependencyResolver.prototype.onresolve = function(data){
+	this.dependencies[this.index - 1] = data;
+	this.next();
+};
 
-	require.cache = {};
-	//require.Resolver = Resolver;
+DependencyResolver.prototype.onend = function(){
+	
+};
 
-	return require;
+function require(){
+	if( this instanceof DependencyResolver ){
+		return DependencyResolver.prototype.constructor.apply(this, arguments);
+	}
+	else{
+		return new require(Array.apply(Array, arguments));
+	}
+}
 
-})();
+require.prototype = Object.create(DependencyResolver.prototype);
+require.prototype.constructor = require;
+require.prototype.cache = require.cache = {};
+
+require.prototype.setId = function(id){
+	// use require.alias here	
+	this.id = require.fileURL(id);
+};
+require.prototype.getId = function(id){
+	return this.id;
+};
+require.prototype.resolve = function(id){
+	require.currentResolver = this;
+	require.loadFile(id);
+};
+require.prototype.onend = function(id){
+	require.currentResolver = require.prototype;
+};
+
+require.currentResolver = require.prototype;
+
+function provide(){
+	require.currentResolver.provide.apply(require.currentResolver, arguments);
+}
+
+require.alias = {};
+
+require.config = {
+	async: true,
+	root: './',
+	extension: 'js',
+	charset: 'utf8'
+};
+
+require.filepath = function(name){
+	return require.config.extension + '/' + name + '.' + require.config.extension;
+};
+
+require.fileURL = function(name){
+	return require.config.root + require.filepath(name, require.config.extension);
+};
+
+require.loadFile = function(path, callback){
+	var type, element;
+
+	if( path.match(/.js$/) ){
+		type = 'js';
+		element = document.createElement('script');
+		element.type = 'text/javascript';
+		element.charset = require.config.charset;
+		element.async = require.config.async;
+	}
+	else if( path.match(/.css$/) ){
+		type = 'css';
+		element = document.createElement('link');
+		element.type = 'text/css';
+		element.rel = 'stylesheet';
+	}
+	else{
+		throw new Error('unsupported file extension');
+	}
+
+	if( typeof callback == 'function' ){
+		element.onerror = callback;
+		element.onload = callback;
+	}
+
+	document.head.appendChild(element);
+
+	if( type == 'js' ) element.src = path;
+	else element.href = path;
+};
+
+require.loadFiles = function(names, extension){
+	var i = 0, j = names.length, prevExt = require.config.extension, prevAsync = require.config.async;
+
+	require.config.extension = extension;
+	require.config.async = false;
+	for(;i<j;i++){
+		require.loadFile(require.fileURL(names[i]));
+	}
+	require.config.extension = prevExt;
+	require.config.async = prevAsync;
+
+};
+
+require.id = function(id){
+	require.prototype.setId(id);
+};
+
+/*
+en écrivant direct comme ça on peut concaténer les fichiers puisque le fichier et ce qu'il provide est
+immédiatement stocké
+
+require.id("superdependency");
+provide({
+	superdependency: true
+});
+
+require.id("dependency");
+provide('dependency', function(superdependency){
+	superdependency.dependency = true;
+	return superdependency;
+});
+
+require.id("provide");
+provide('superdependency', function(superdependency){
+	superdependency.dependency = true;
+	return superdependency;
+});
+*/
+
+
