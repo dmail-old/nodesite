3c5d4658dbeddf5ac9287912c794dcb7c5dafcf3
Damien Maillard <Angelblade@hotmail.fr>
2014-04-09 23:33:44 +0200

    spec on client modules progress

--- app/client/js/module.js
+++ app/client/js/module.js
@@ -6,41 +6,65 @@ https://github.com/joyent/node/blob/master/src/node.js
 
 */
 
-var Module = function(id, parent){
-	this.id = id;
-	this.parent = parent;
+var Module = function(filename, parent){
+	this.filename = filename; // fully resolved filename
+	this.parent = parent; // parent all have on except main module
+	this.resolvedPaths = {}; // contain path already resolved to filenames for this module
 };
 
 Module.prototype = {
-	// marche pas parce que selon d'ou je require l'id ne signifie pas la même chose
-	// si je require depuis mainmodule 'app' signifie pas pareil que si je require depuis un autre module
-	// est ce qu'on cherche un module pour require('app/ok.js') dans 'app/node_modules/ok.js??'
-	resolvedIds: {},
 	cache: {},
-	path: null, // fully resolved path
 	source: null,
 	exports: null,
+	async: false,
 	
+	_resolve: function(){
+		var xhr = new XMLHttpRequest();
+
+		xhr.open('GET', window.location.origin + '/require', this.async);
+		xhr.setRequestHeader('x-required-by', this.parent.filename);
+		xhr.setRequestHeader('x-require', this.filename);
+		xhr.send(null);
+
+		return xhr.responseText;
+	},
+
+	resolve: function(path){
+		if( path in this.resolvedPaths ){
+			return this.resolvedPaths[path];
+		}
+		else{
+			return this.resolvedPaths[path] = this._resolve(path);
+		}
+	},
+
+	createChild: function(url){
+		return new Module(url, this);
+	},
+
 	_load: function(){
 		var xhr = new XMLHttpRequest();
 
-		xhr.open('GET', window.location.origin, false);
-		xhr.setRequestHeader('x-required-by', this.parent.id);
-		xhr.setRequestHeader('x-require', this.id);
+		xhr.open('GET', window.location.origin + '/require', this.async);
+		xhr.setRequestHeader('x-required-by', this.parent.filename);
+		xhr.setRequestHeader('x-require', this.filename);
 		xhr.send(null);
 
-		if( xhr.status >= 200 || this.status < 400 ){
-			this.source = xhr.responseText;
-			this.path = xhr.getResponseHeader('x-module-path');		
-			return this.source;
-		}
-
-		throw new Error('not found');
+		return xhr;
 	},
 
 	load: function(){
 		if( this.source ) return this.source;
-		return this.source = this._load();
+
+		var xhr = this._load(false);
+		if( xhr.status >= 200 || this.status < 400 ){
+			this.source = xhr.responseText;	
+		}
+		else{
+			throw new Error('not found');
+		}		
+
+		return this.source;
 	},
 
 	eval: function(source, url){
@@ -50,7 +74,6 @@ Module.prototype = {
 
 	compile: function(){
 		if( !this.hasOwnProperty('exports') ){
-
 			var source, fn;
 
 			source = this.load();
@@ -73,45 +96,21 @@ Module.prototype = {
 				throw e;
 			}
 		}
+
+		return this.exports;
 	},
 
-	createChild: function(url){
-		return new Module(url, this);
-	},
+	require: function(path){
+		var filename = this.resolve(path);
 
-	resolve: function(id){
-		if( id in this.resolvedIds ){
-			return this.resolvedIds[id];
-		}
-		return null;
-	},
-
-	require: function(id){
-		var path = this.resolve(id);
-
-		if( path ){
-			if( path in this.cache ){
-				module = this.cache[path];
-			}
-			else{
-				// bizarre non?
-			}
+		if( filename in this.cache ){
+			module = this.cache[filename];
 		}
 		else{
-			module = this.createChild(id);
-			module._load();
-			this.resolvedIds[module.id] = module.path;
-			this.cache[module.path] = module;
+			module = this.cache[filename] = this.createChild(filename);
 		}
-
-		// may throw different errors as not found, syntax error and more
-		module.compile();
-
-		return module.exports;
-	},
-
-	get filename(){
-		return this.path;
+		
+		return module.compile();
 	},
 
 	get dirname(){
@@ -133,7 +132,6 @@ Module.prototype = {
 
 // main module
 window.module = new Module(window.location.origin.pathname);
-window.module.path = window.module.id;
 window.require = window.module.require.bind(window.module);
 window.__filename = window.module.filename;
 window.__dirname = window.module.dirname;
