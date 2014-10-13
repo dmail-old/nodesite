b6ff01a32b42c1cec21dd5642571f55aaada15b5
Damien Maillard <Angelblade@hotmail.fr>
2014-10-13 23:49:34 +0200

    Adding evaluation of user input to admin terminal

--- admin.js
+++ admin.js
@@ -181,3 +181,23 @@ logger.info('Node.js version {version} running on {platform}', process.version, 
 nodeServer.start();
 
 //require('fs').createReadStream('./notes.txt').pipe(logger, {end: false});
+
+var readline = require('readline'), rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.setPrompt('admin> ');
+rl.prompt();
+rl.on('line', function(line){
+	var code = line.trim();
+
+	try{
+		console.log(eval(code));
+	}
+	catch(e){
+		console.log(e.stack);
+	}
+
+	rl.prompt();
+}).on('close', function() {
+	console.log('Have a great day!');
+	process.exit(0);
+});
